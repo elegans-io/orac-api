c5b29b57e54a3cb1b4086fe6bd6c59e9e7617e3b
Angelo Leto <angleto@gmail.com>
2018-04-05 12:58:50 +0200

    Revert "changed recommendation/query to recommendation_query"
    This reverts commit 26c5e8ae5da7f0613cadff3209829ffba80cfcfe.

--- scripts/api_test/recommendation/deleteRecommendationByQuery.sh
+++ scripts/api_test/recommendation/deleteRecommendationByQuery.sh
@@ -7,5 +7,5 @@ TO=${4:-1522755010000}
 
 # delete one or more entries with given ids; ids can be specified multiple times
 curl -v -H "Authorization: Basic `echo -n 'test_user:p4ssw0rd' | base64`" \
-  -H "Content-Type: application/json" -X DELETE "http://localhost:${PORT}/${INDEX_NAME}/recommendation_query?from=${FROM}&to=${TO}"
+  -H "Content-Type: application/json" -X DELETE "http://localhost:${PORT}/${INDEX_NAME}/recommendation/query?from=${FROM}&to=${TO}"
 
